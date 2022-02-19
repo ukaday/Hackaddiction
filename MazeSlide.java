bc63a3f84fbb95b10bee9a0bee5ec69bf1b63a39
ukaday <73198386+ukaday@users.noreply.github.com>
2022-02-19 13:48:41 -0600

    added player movement, collision, maze cycle

--- MazeSlide.java
+++ MazeSlide.java
@@ -1,5 +1,6 @@
-import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class MazeSlide{
     
@@ -9,6 +10,58 @@ public class MazeSlide{
     public MazeSlide(int[][] m) {
         maze = m;
         mazeSize = m.length;
+    }
+
+    //player is stored in array as integer 2
+    public void addPlayer(int x, int y) {
+        maze[y][x] = 2;
+    }
+
+    //takes x and y, turns back to 0
+    public void removePlayer(int x, int y) {
+        maze[y][x] = 0;
+    }
+
+    //checks if a postion is a 1 or 0 
+    public boolean possibleMove(int x, int y) {
+        if (maze[y][x] == 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getMazeSize() {
+        return mazeSize;
+    }
+
+    public boolean containsPlayer() {
+        for (int[] row : maze) {
+            for (int col : row) {
+                if (col == 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void draw(Graphics g) {
+        int y = 0;
+        for (int i = 0; i < mazeSize; i++) {
+            int x = 0;
+            for (int j = 0; j < mazeSize; j++) {
+                if (maze[i][j] == 1) {
+                    g.setColor(Color.white);
+                    g.fillRect(x, y, 25, 25);
+                }
+                else if (maze[i][j] == 2) {
+                    g.setColor(Color.CYAN);
+                    g.fillRect(x, y, 25, 25);
+                }
+                x += 25;
+            }
+            y += 25;
+        }
     }
 
     public void printMaze() {
@@ -24,27 +77,4 @@ public class MazeSlide{
             System.out.println(line);
         }
     }
-  
-
-    public void update() {
-    }
-
-    /*int[][] slide = {
-        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
-        {1, 0, 1, 1, 1, 0, 1, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 1, 0, 1},
-        {1, 0, 1, 1, 1, 0, 1, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
-
-        
-    MazeSlide maze1 = new MazeSlide(slide);
-
-    maze1.printMaze(); */
-
-  
 }
--- Panel.java
+++ Panel.java
@@ -5,35 +5,104 @@ import java.util.Random;
 
 public class Panel extends JPanel implements KeyListener{
 
+    enum State {
+        LEVEL_ONE,
+        LEVEL_INFINITE,
+    }
+
     final int SCREEN_WIDTH = 800;
     final int SCREEN_HEIGHT = 800;
-    Timer timer;
-    Random random;
+    State gameState;
+
+    //timer stuff, fps is how many times it runs per second
+    final int fps = 20;
+    final double period = 1000000000 / fps;
+    double delta = 0;
+    long now;
+    long lastTime = System.nanoTime();
+
+    MazeSlide currentMaze;
+    MazeContainer container;
+
+    int x = 100;
+    int y = 100;
 
     public Panel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(this);
+
+        container = new MazeContainer();
     }
 
     public void start() {
+        gameState = State.LEVEL_ONE;
+        container = new MazeContainer();
     }
 
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
 
+    //this is the looping function
+    //i have the update function running 'fps' times per second
     public void draw(Graphics g) {
+
+        //update functions go here
+        now = System.nanoTime();
+        delta += (now - lastTime) / period;
+        lastTime = now;
+
+        if (delta >= 1) {
+            container.update();
+            delta--;
+        }
+
+        container.draw(g);
+
+        repaint();
     }
 
     public void keyTyped(KeyEvent e) {
     }
 
     public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case 65:
+                container.setVelX(-1);
+                break;
+            case 68:
+                container.setVelX(1);
+                break;
+            case 87:
+                container.setVelY(-1);
+                break;
+            case 83:
+                container.setVelY(1);
+                break;
+            default:
+                break;
+        }
     }
 
     public void keyReleased(KeyEvent e) {
-        System.out.println(e.getKeyCode());
-        
+        switch (e.getKeyCode()) {
+            case 65:
+                container.setVelX(0);
+                break;
+            case 68:
+                container.setVelX(0);
+                break;
+            case 87:
+                container.setVelY(0);
+                break;
+            case 83:
+                container.setVelY(0);
+                break;
+            default:
+                break;
+        }
     }
 }
